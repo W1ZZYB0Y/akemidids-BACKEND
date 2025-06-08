db49b30abaaa4b753dfce2a943058b320053cd87
W1ZZYB0Y <udomwisdom466@gmail.com>
2025-06-08 14:26:36 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true, unique: true },
-  username: String,
-  balance: { type: Number, default: 0 },
-  rank: { type: String, default: "Dwarf Lantern shark" },
+const userSchema = new mongoose.Schema({ 
+  telegramId: { type: String, unique: true, sparse: true },
+  username: { type: String, required: true, unique: true },
+  balance: { type: Number, default: 0 }, rank: { type: String, default: "Dwarf Lantern shark" },
   clickLimit: { type: Number, default: 50 },
   clicks: { type: Number, default: 0 },
-  ip: String,
-  referrals: [{ type: String }],
-  referredBy: String,
+  ip: String, referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   tasksCompleted: [String],
-  referralRewards: { type: Number, default: 0 }
+  referralRewards: { type: Number, default: 0 },
+  referralDetails: [{ 
+    referredUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    reward: Number, 
+    date: Date 
+  }] 
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
