ac7247c3b2d72bb850d5fdafc1daa0561dbe8411
W1ZZYB0Y <udomwisdom466@gmail.com>
2025-06-25 17:09:26 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,20 +1,37 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({ 
-  telegramId: { type: String, unique: true, sparse: true },
-  username: { type: String, required: true, unique: true },
-  balance: { type: Number, default: 0 }, rank: { type: String, default: "Dwarf Lantern shark" },
-  clickLimit: { type: Number, default: 50 },
-  clicks: { type: Number, default: 0 },
-  ip: String, referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  tasksCompleted: [String],
-  referralRewards: { type: Number, default: 0 },
-  referralDetails: [{ 
-    referredUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    reward: Number, 
-    date: Date 
-  }] 
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    telegramId: { type: String, unique: true, sparse: true }, // optional, will be empty for some
+    username: { type: String, required: true, unique: true }, // username is unique and required
+    balance: { type: Number, default: 0 },
+    rank: { type: String, default: "Dwarf Lantern shark" },
+    clickLimit: { type: Number, default: 50 },
+    clicks: { type: Number, default: 0 },
+    ip: { type: String, default: '' },
 
-module.exports = mongoose.model("User", userSchema);
+    // Direct list of all referred user IDs
+    referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+
+    // Parent referrer who referred this user
+    referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
+    // Track completed tasks
+    tasksCompleted: { type: [String], default: [] },
+
+    // Accumulated rewards from referral bonuses
+    referralRewards: { type: Number, default: 0 },
+
+    // Detailed referral information
+    referralDetails: [
+      {
+        referredUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        reward: { type: Number, default: 0 },
+        date: { type: Date, default: Date.now }
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
